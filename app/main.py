d5982305cd877addab5b82e519bfea672915f9eb
Rodwan26 <rdwanbnmwsy064@gmail.com>
2026-08-09 05:52:38 +0100

    Fix PostgreSQL driver

--- app/main.py
+++ app/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://front-algeria-commerce.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
